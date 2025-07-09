502a19b8269555631bd9464573d49c238feddf82
dylanndo <62081958+dylanndo@users.noreply.github.com>
2025-07-09 11:02:05 -0400

    Create Daemon directory and file

---
+++ selfdrive/v2v/v2vd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import time
+
+def main():
+  # Your daemon's logic will go here
+  while True:
+    print("v2vd running...")
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
